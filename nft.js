814a2ad55b3a5647c7baf2ae77615ad2262ee5ae
Tomás Rossi <91207352+rossitom90@users.noreply.github.com>
2022-09-04 12:03:36 -0300

    update9

--- nft.js
+++ nft.js
@@ -19,25 +19,34 @@ fetch('/nft.json')
             <div class="nftImg01">
                 <img src="${nftTarjeta.img}" class="imagenNftCard">
 
-                <div class="creador">
-                    <img src="${nftTarjeta.imgCreator}" class="imgCreador">
-                    <div class="texto-creador">
-                        <p class="creador-descrip">Creador</p>
-                        <p class="creador-nombre">${nftTarjeta.creatorName}</p>
+                    <div class="creador">
+                        <img src="${nftTarjeta.imgCreator}" class="imgCreador">
+                            <div class="texto-creador">
+                                <p class="creador-descrip">Creador</p>
+                                <p class="creador-nombre">${nftTarjeta.creatorName}</p>
+                            </div>
                     </div>
-                </div>
-            </div>
 
-            <div class="fondo_card">
+                <div class="fondo_card">
                     <div class="nombrenftdiv">
                         <p class="nombre-nft">${nftTarjeta.name}</p>
                     </div>
 
-                <div class="casilla-precio">
-                    <p class="price-crypto">${nftTarjeta.lastSale}</p>
-                    <button class="ver" id="${nftTarjeta.id}">ver</button>
-                </div>
-             </div> 
+                    <div class="casilla-precio">
+
+                        <div class="seccionprccrypto">
+                            <p class="price-crypto">${nftTarjeta.lastSale}</p>
+                            <p class="blockCard">${nftTarjeta.unit}</p>
+                        </div>
+                        
+                        <button class="ver" id="${nftTarjeta.id}">ver</button>
+                     </div>
+                     
+                </div> 
+            </div>
+
+            
+
         </div>
         `
       contenedorT.append(tarjeta);
